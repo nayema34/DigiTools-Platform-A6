56ff21952ae71dc4af080eae7753fcd13539de81
nayema34 <jannatnayema2@gmail.com>
2026-03-31 22:00:36 +0600

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Navbar from './Components/NavBar'
 
 function App() {
  
   return (
    <>
-    <div>
-      <p className='text-6xl'>hello</p>
-    </div>
+    <Navbar/>
    </>
   )
 }
